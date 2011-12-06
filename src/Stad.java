9a6daf1be770c26c67019944e41c0f8df184780c
Fredrik Lundin <lundin_fredrik@hotmail.com>
2011-12-06 16:04:04 +0100

    first commit

---
+++ src/Stad.java
@@ -0,0 +1,21 @@
+
+public class Stad
+{
+	private String namn;
+
+	public Stad(String namn)
+	{
+		this.namn = namn;
+	}
+
+	public String getNamn()
+	{
+		return namn;
+	}
+
+	public String toString()
+	{
+		return namn;
+	}
+
+}
